36d4a36d94826d9bce51ac4f925ce9108216c1f2
Kelvin Amoo <ykelvin46@gmail.com>
2022-03-13 11:18:05 +0000

    firebaselogin

---
+++ lib/login.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+final auth = FirebaseAuth.instance;
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  TextEditingController emailControllar = TextEditingController();
+  TextEditingController passwordControllar = TextEditingController();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            TextField(
+              controller: emailControllar,
+              decoration: InputDecoration(
+                  hintText: 'Email', prefixIcon: Icon(Icons.email)),
+            ),
+            TextField(
+              controller: passwordControllar,
+              decoration: InputDecoration(
+                  hintText: 'Password', prefixIcon: Icon(Icons.lock)),
+            ),
+            ElevatedButton(
+              onPressed: ()  async {
+                try{
+                  final currentuser = await auth.signInWithEmailAndPassword
+                    (email: emailControllar.text,  password: passwordControllar.text, );
+                if(currentuser != null){
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (_)=>MyHomePage(),),);
+                }
+                }on FirebaseAuthException catch(e){
+                  print(e.message.toString());
+                  }
+              },
+              child: Text('submit'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
